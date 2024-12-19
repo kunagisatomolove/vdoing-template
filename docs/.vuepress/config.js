546dbabd042a88cdb2f089db3344986eef578943
王建 <2200189658@qq.com>
2024-12-19 20:31:21 +0800

    提交

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -14,4 +14,21 @@ module.exports = {
   head,
   plugins,
   themeConfig,
+  // vssue 评论插件
+  plugins: [
+    [
+      "vuepress-plugin-vssue-global",
+      {
+        platform: "github",
+        title: "[Comment]<%- frontmatter.title %>",
+        needComments: true,
+        // 其他的 Vssue 配置
+        autoCreateIssue: true,
+        clientId: "Ov23liUjkZYkkK2UPax4",
+        clientSecret: "02e1e7c84509357562df1d3c60c40759cddd4554",
+        owner: "kunagisatomolove",
+        repo: "vdoing-template",
+      },
+    ],
+  ],
 }
