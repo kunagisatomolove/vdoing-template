bedbceb5b39b722f65e74ef6b51844f25485c196
王建 <2200189658@qq.com>
2024-12-20 13:11:43 +0800

    t

--- docs/.vuepress/config/themeConfig/nav.js
+++ docs/.vuepress/config/themeConfig/nav.js
@@ -1,9 +1,9 @@
 // nav
 module.exports = [
   { text: '首页', link: '/' },
-  { text: 'one',link: '/one/',},
-  { text: 'two',link: '/two/',},
-  { text: 'three', link: '/three/' },
+  { text: 'Java',link: '/java/',},
+  { text: 'C++',link: '/two/',},
+  { text: '区块链', link: '/knowledge/' },
   { text: '留言板', link: '/message-board/' },
   { text: '我的博客', link: 'https://wiki.eryajf.net' },
 ]
